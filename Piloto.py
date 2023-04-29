6aca3cb76762a9b207f819d9fb287701507dc099
shadow <mnltobon@gmail.com>
2023-04-29 12:13:07 -0500

    Se arregla identacion

--- Piloto.py
+++ Piloto.py
@@ -1,40 +1,38 @@
 class Piloto:
     def __init__(self):
-        self.__nombre= None
-        self.__fechaNacimiento= None
-        self.__salarioAnual= None
-        self.__Pais= None
+        self.__nombre = None
+        self.__fechaNacimiento = None
+        self.__salarioAnual = None
+        self.__Pais = None
 
     @property
     def nombre(self):
         return self.__nombre
-    
+
     @nombre.setter
-    def nombre(self,dato):
-        self.__nombre=dato
-    
-     @property
+    def nombre(self, dato):
+        self.__nombre = dato
+
+    @property
     def fechaNacimiento(self):
         return self.__fechaNacimiento
-    
+
     @fechaNacimiento.setter
-    def fechaNacimiento(self,dato):
-        self.__fechaNacimiento=dato
-    
-     @property
+    def fechaNacimiento(self, dato):
+        self.__fechaNacimiento = dato
+
+    @property
     def salarioAnual(self):
         return self.__salarioAnual
-    
+
     @salarioAnual.setter
-    def salarioAnual(self,dato):
-        self.__salarioAnual=dato
-    
-     @property
+    def salarioAnual(self, dato):
+        self.__salarioAnual = dato
+
+    @property
     def Pais(self):
         return self.__pais
-    
-    @Pais.setter
-    def Pais(self,dato):
-        self.__Pais=dato
 
-    
+    @Pais.setter
+    def Pais(self, dato):
+        self.__Pais = dato
